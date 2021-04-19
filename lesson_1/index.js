0e3c921f94768e2fe028be7072dffa8cf7e7da6c
andrewmatanov <60537186+Andrey-Matanov@users.noreply.github.com>
2021-04-19 14:50:38 +0500

    ДЗ № 1 полностью готово

--- lesson_1/index.js
+++ lesson_1/index.js
@@ -1,7 +1,31 @@
-const { PrimeNumbersLogger } = require("./src/PrimeNumbersLogger");
+const colors = require("colors/safe");
+const PrimeNumbersLogger = require("./src/PrimeNumbersLogger");
 
-const start = +process.argv[2];
-const end = +process.argv[3];
-const logger = new PrimeNumbersLogger(start, end);
+const arguments = process.argv;
 
-logger.logPrimeNumbers();
+if (arguments.length === 4) {
+    const first = +arguments[2];
+    const second = +arguments[3];
+
+    if (!Number.isNaN(first) && !Number.isNaN(second)) {
+        const logger = new PrimeNumbersLogger(first, second);
+        logger.logPrimeNumbers();
+    } else {
+        console.log(colors.red("Оба аргумента должны являться числом"));
+    }
+} else if (arguments.length === 3) {
+    const first = +arguments[2];
+
+    if (!Number.isNaN(first)) {
+        const logger = new PrimeNumbersLogger(2, first);
+        logger.logPrimeNumbers();
+    } else {
+        console.log(colors.red("Аргумент должен являться числом"));
+    }
+} else {
+    console.log(
+        colors.red(
+            "Неверное число аргументов. В качестве аргументов используйте одно число number для поиска простых чисел в диапазоне [2; number], либо два числа number1 и number2 для поиска простых чисел в диапазоне [number1; number2]"
+        )
+    );
+}
--- lesson_1/src/PrimeNumbersLogger.js
+++ lesson_1/src/PrimeNumbersLogger.js
@@ -1,24 +1,38 @@
 const colors = require("colors/safe");
 
 class PrimeNumbersLogger {
+    // this.start - Левый предел числового диапазона чисел
+    // this.end - Правый предел числового диапазона чисел
+    // this.primeNumberWasFound - Показывает, что хотя бы одно простое число было найдено
+
     constructor(start, end) {
-        this.start = start; // Левый предел диапозона чисел
-        this.end = end; // Правый предел диапозона чисел
+        this.start = start;
+        this.end = end;
+        this.primeNumberWasFound = false;
     }
 
     isPrime(number) {
         // Проверяет что число является простым
+
+        if (number < 2) {
+            return false;
+        }
+
         for (let i = 2; i <= Math.sqrt(number); i++) {
             if (number % i === 0) {
                 return false;
             }
         }
 
+        this.primeNumberWasFound = true;
         return true;
     }
 
     logPrimeNumbers() {
-        // Выводит простые числа из заданного диапозона чисел в консоль, окрашивая их в красный, желтый, зеленый цвета
+        // Выводит простые числа из заданного диапазона чисел в консоль, окрашивая их в красный, желтый, зеленый цвета
+
+        console.log(`Заданный диапазон чисел: [${this.start}, ${this.end}]`);
+
         let counter = 0;
         let current = this.start;
 
@@ -36,9 +50,13 @@ class PrimeNumbersLogger {
                 current++;
             }
         }
+
+        if (this.primeNumberWasFound === false) {
+            console.log(
+                colors.red("В пределах заданного диапазона чисел нет простых")
+            );
+        }
     }
 }
 
-module.exports = {
-    PrimeNumbersLogger,
-};
+module.exports = PrimeNumbersLogger;
