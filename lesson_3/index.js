deb802a91153b8b8b23db9eb1d1d7c262d4e9371
andrewmatanov <60537186+Andrey-Matanov@users.noreply.github.com>
2021-05-06 13:16:08 +0500

    Доделал ДЗ № 3 до конца

--- lesson_3/index.js
+++ lesson_3/index.js
@@ -6,12 +6,12 @@ const ora = require("ora");
  * Построчно считывает файл access.log, ищет все логи, относящиеся к ip-адресам 89.123.1.41 и 34.48.240.111, и записывает эти логи в файл test.log. Если test.log не существует, он автоматически создается.
  */
 async function readLogLineByLine() {
-    const fileStream = fs.createReadStream("access.log", { flags: "r" });
+    const fileStream = fs.createReadStream("access1.log", { flags: "r" });
     const rl = readline.createInterface({
         input: fileStream,
     });
     const spinner = ora(
-        "Файл считывается и необходимые логи записываются в файл test.log\n\nОбработано 0 строк"
+        "Файл считывается и необходимые логи записываются в соответствующие файлы\n\nОбработано 0 строк"
     ).start();
 
     let count = 0;
@@ -19,7 +19,7 @@ async function readLogLineByLine() {
 
     const setSpinnerText = () => {
         interval = setTimeout(() => {
-            spinner.text = `Файл считывается и необходимые логи записываются в файл test.log\n\nОбработано ${count} строк`;
+            spinner.text = `Файл считывается и необходимые логи записываются в соответствующие файлы.log\n\nОбработано ${count} строк`;
 
             if (last !== count) {
                 setSpinnerText();
@@ -28,6 +28,19 @@ async function readLogLineByLine() {
                 spinner.succeed("Обработка файла успешно завершена!");
             }
         }, 1000);
+    };
+
+    const writeLogs = (ip, line) => {
+        fs.writeFileSync(
+            `${ip}_requests.log`,
+            `${line}\n`,
+            { flag: "as" },
+            (err) => {
+                if (err) {
+                    console.log(err);
+                }
+            }
+        );
     };
 
     setSpinnerText();
@@ -39,11 +52,11 @@ async function readLogLineByLine() {
             line.startsWith("89.123.1.41") ||
             line.startsWith("34.48.240.111")
         ) {
-            fs.writeFileSync("test.log", `${line}\n`, { flag: "as" }, (err) => {
-                if (err) {
-                    console.log(err);
-                }
-            });
+            if (line.startsWith("89.123.1.41")) {
+                writeLogs("89.123.1.41", line);
+            } else {
+                writeLogs("34.48.240.111", line);
+            }
         }
     }
 }
